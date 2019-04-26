27b90b9966a5547bd7ec65dd49718fb5c0684e18
Min Liu <wzygzlm@gmail.com>
2019-04-26 20:43:41 +0200

    Added a script used to download DVS MNIST dataset automatically.

---
+++ data/DVS_MNIST/downloadDVSMnist.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+mkdir DVS_MNIST
+cd DVS_MNIST
+wget http://www2.imse-cnm.csic.es/caviar/MNIST_DVS/grabbed_data0.zip
+wget http://www2.imse-cnm.csic.es/caviar/MNIST_DVS/grabbed_data1.zip
+wget http://www2.imse-cnm.csic.es/caviar/MNIST_DVS/grabbed_data2.zip
+wget http://www2.imse-cnm.csic.es/caviar/MNIST_DVS/grabbed_data3.zip
+wget http://www2.imse-cnm.csic.es/caviar/MNIST_DVS/grabbed_data4.zip
+wget http://www2.imse-cnm.csic.es/caviar/MNIST_DVS/grabbed_data5.zip
+wget http://www2.imse-cnm.csic.es/caviar/MNIST_DVS/grabbed_data6.zip
+wget http://www2.imse-cnm.csic.es/caviar/MNIST_DVS/grabbed_data7.zip
+wget http://www2.imse-cnm.csic.es/caviar/MNIST_DVS/grabbed_data8.zip
+wget http://www2.imse-cnm.csic.es/caviar/MNIST_DVS/grabbed_data9.zip
+wget http://www2.imse-cnm.csic.es/caviar/MNIST_DVS/dat2mat.m
+wget http://www2.imse-cnm.csic.es/caviar/MNIST_DVS/mat2dat.m
+wget http://www2.imse-cnm.csic.es/caviar/MNIST_DVS/process_mnist.m
+wget http://www2.imse-cnm.csic.es/caviar/MNIST_DVS/readme.txt
